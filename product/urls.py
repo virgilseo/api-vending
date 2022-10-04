2c78bf9b41d8d268c3caff294a9608c6907f575d
Virgil Avram <virgilavram.dev@gmail.com>
2022-10-04 14:14:33 +0200

    Add product urls

---
+++ product/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . views import *
+
+urlpatterns = [
+    path('products', ProductListView.as_view()),
+    path('products/<int:id>', ProductDetailView.as_view())
+]
+ 
--- product/views.py
+++ product/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from . models import *
+from .serializers import*
+from rest_framework import generics
 
 # Create your views here.
+
+class ProductListView(generics.ListAPIView, generics.CreateAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+    lookup_field = 'id'
+    
+
+class ProductDetailView(generics.RetrieveAPIView,
+                        generics.DestroyAPIView,
+                        generics.UpdateAPIView):
+      
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+    lookup_field = 'id'
