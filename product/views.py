42553117b34544b4d8ed69fd98eccce5b1b91f10
Virgil Avram <virgilavram.dev@gmail.com>
2022-10-05 14:56:51 +0200

    Implement jwt auth for product views

--- product/views.py
+++ product/views.py
@@ -2,19 +2,23 @@ from django.shortcuts import render
 from . models import *
 from .serializers import*
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
 
 class ProductListView(generics.ListAPIView, generics.CreateAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
     lookup_field = 'id'
-    
+    permission_classes = (IsAuthenticated,)
+
 
 class ProductDetailView(generics.RetrieveAPIView,
                         generics.DestroyAPIView,
                         generics.UpdateAPIView):
-      
+    permission_classes = (IsAuthenticated,)
+
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
     lookup_field = 'id'
