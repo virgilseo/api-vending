bdfd1fc508f990936a44074bb8ad267a8e18e84c
Virgil Avram <virgilavram.dev@gmail.com>
2022-10-10 17:19:06 +0200

    Refactor user profile creation

--- user/serializers.py
+++ user/serializers.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
+from rest_framework.response import Response
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -20,13 +21,16 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
+        response = {}
+        profile = validated_data.pop('profile')
+
         user = User.objects.create(
             username=validated_data['username'],
             password=make_password(validated_data['password']))
 
-        UserProfile.objects.create(user=user)
-        UserProfile.objects.filter(user=user).update(
-            role=validated_data['profile']['role']
+        UserProfile.objects.create(
+            user=user,
+            role=profile['role']
         )
 
         return user
